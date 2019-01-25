9349f67cd12fc1c3df71c7a0cda981e41be02cf1
pouget <pouget@pouget-ordi>
2019-01-25 11:25:30 +0100

    Push contenu classe Produit et ProduitManquant

--- Element/element/Produit.java
+++ Element/element/Produit.java
@@ -1,5 +1,15 @@
 package element;
 
 public class Produit extends Element{
-	
+	/**
+	 * Constructeur de produit
+	 * @param c code d'un produit : String
+	 * @param n nom d'un produit : String
+	 * @param q quantité d'un produit : double
+	 * @param m mesure d'un produit : String
+	 * @param p prix de vente d'un produit : Int
+	 */
+	public Produit(String c, String n, double q, String m, double p) {
+		super(c, n, q, m, p);
+	}
 }
--- Element/element/ProduitManquant.java
+++ Element/element/ProduitManquant.java
@@ -1,5 +1,28 @@
 package element;
 
 public class ProduitManquant extends Produit{
+	
 	private double qteP;
+	/**
+	 * Constructeur de produit
+	 * @param c code d'un produit : String
+	 * @param n nom d'un produit : String
+	 * @param q quantité d'un produit : double
+	 * @param m mesure d'un produit : String
+	 * @param p prix de vente d'un produit : Int
+	 * @param qM quantité manquante d'un produit
+	 */
+	public ProduitManquant(String c, String n, double q, String m, double p, double qM) {
+		super(c, n, q, m, p);
+		this.qteP =qM;
+	}
+	
+	/**
+	 * 
+	 * @return la quantité manquante du produit : double
+	 */
+	public double getQuantiteM() {
+		return this.qteP;
+	}
+	
 }
