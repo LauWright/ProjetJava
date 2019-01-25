01881d045fa0a862745c52772d4fbf529e08df0f
pouget <pouget@pouget-ordi>
2019-01-25 11:43:45 +0100

    Constructeur accesseur ChaineProduction + Couple

--- Element/production/ChaineProduction.java
+++ Element/production/ChaineProduction.java
@@ -5,21 +5,58 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
+/**
+ * @author pouget
+ *
+ */
 public class ChaineProduction {
 	private String code;
 	private String nom;
 	private List<Couple> entrees;
 	private List<Couple> sorties;
 	
-	public ChaineProduction(String nomCsv, String separateur) {
-
-		
-        
-        
-		
-		
+	/**
+	 * Constructeur d'une chaine de production
+	 * @param code d'une chaine de production : String
+	 * @param nom d'une chaine de production : String
+	 * @param entrees éléments nécessaires pour effectuer chaine de production List<Couple>
+	 * @param sorties produits sortants de la chaine : List<Couple>
+	 */
+	public ChaineProduction(String code, String nom, List<Couple> entrees, List<Couple> sorties) {
+		this.code = code;
+		this.nom = nom;
+		this.entrees = entrees;
+		this.sorties = sorties;
 	}
-		
+
+	/**
+	 * @return le code de la chaine de production
+	 */
+	public String getCode() {
+		return code;
+	}
+	
+	/**
+	 * @return le nom de la chaine de production
+	 */
+	public String getNom() {
+		return nom;
+	}
+	
+	/**
+	 * @return les éléments nécessaires pour effectuer chaine de production
+	 */
+	public List<Couple> getEntrees() {
+		return entrees;
+	}
+	
+	/**
+	 * @return les produits sortant de la chaine de production
+	 */
+	public List<Couple> getSorties() {
+		return sorties;
+	}
+	
 }
 
 
--- Element/production/Couple.java
+++ Element/production/Couple.java
@@ -4,15 +4,26 @@ public class Couple {
 	private String code;
 	private double qte;
 	
+	/**
+	 * Constructeur du couple
+	 * @param c code d'un élément : String
+	 * @param qte quantité d'éléments nécessaires : double
+	 */
 	public Couple(String c, double qte) {
 		this.code = c;
 		this.qte = qte;
 	}
-
+	/**
+	 * 
+	 * @return le code d'un élément
+	 */
 	public String getCode() {
 		return code;
 	}
-
+	/**
+	 * 
+	 * @return la quantité nécessaire de cet élément
+	 */
 	public double getQte() {
 		return qte;
 	}
